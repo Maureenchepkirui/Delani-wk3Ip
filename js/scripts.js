86c62160652d5b3e2be3d706e16bcfd3cd25b481
maureenchepkirui <maureenchepkirui005@gmail.com>
2020-05-11 10:57:45 +0300

    Add javascript library

--- js/scripts.js
+++ js/scripts.js
@@ -5,34 +5,26 @@ $(document).ready(function(){
         $(this).children('.show').toggle();
       });
 
-    $('.card').hover(function(event){
-      $(".card").hover(function () {
-        $(this).children(".card-1").fadeToggle(1500);
-          $(this).children('.show').toggle();
+
+        $('.card').hover(function(event){
+          $(".card").hover(function () {
+            $(this).children(".card-1").fadeToggle('fast', "linear");
+          });
+          });
 
 
-      });
-      });
-      event.preventDefault();
+
+        $('form#myForm').submit(function(){
+          var name = $('#name').val();
+          var pass = $('#email').val();
+          var mess = $('#mess').val();
+          alert("Hi " +name+ " we have received your message and we will get in touch. Thank you for contacting us.");
+        });
+
+        $("button").on('click', function(){
+          alert("Hi " +name+ " we have received your message and we will get in touch. Thank you for contacting us.");
+        });
 
 
-    $('form#myForm').submit(function(){
-      var Name = $('#mce-FNAME').val();
-        var Email = $('#mce-EMAIL').val();
-        var Message = $('#mce-MESSAGE');
-        var key ='c8fbd9a0cf3958e02be2822e55cfbfc5-us4';
-        if (Name == '' || Email == '' || Message == '') {
-            alert('Please make sure you have filled in the form correctly!');
-        } else {
-            alert(' Hi ' + Name + ' We have received your message. Thank you for reaching out to us.');
-        }
-  });
-
-
-    $("button").on('click', function(){
-      $('form').each(function(){
-        this.reset();
-      });
-
-});
-});
+    });  event.preventDefault();
+    });
